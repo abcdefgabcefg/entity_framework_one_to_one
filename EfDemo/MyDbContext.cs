d09f67705e0570064719dc42b73a19beb358f2f1
abcdefgabcefg <riadinsky.alex@gmail.com>
2020-01-12 11:04:40 +0200

    Create db context

---
+++ EfDemo/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EfDemo.Models;
+using System.Data.Entity;
+
+namespace EfDemo
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<TableWithPrincipalKey> TableWithPrincipalKey { get; set; }
+
+        public DbSet<TableWithForeignKey1> TableWithForeignKey1 { get; set; }
+
+        public DbSet<TableWithForeignKey2> TableWithForeignKey2 { get; set; }
+
+        public MyDbContext() : base("Server=.;Database=MyDatabase;Integrated Security=True;") { }
+    }
+}
